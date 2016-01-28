d190eb0ace85726803aeb60e04bb35e4902e8ba0
sebasdeldi <sebasdeldi123@gmail.com>
2016-01-28 13:39:43 -0500

    Added delete rpoute·

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -33,6 +33,12 @@ class ProductsController < 	ApplicationController
 		end
 	end
 
+	def destroy
+		@product = Product.find(params[:id])
+		@product.destroy
+		redirect_to products_path, notice: "Producto Borrado"
+	end
+
 	private
 		def product_params
 			params.require(:product).permit(:name, :url, :description)
