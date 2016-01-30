7f89935549c62535abe0421e99e2fc0dc5cbdf88
sebasdeldi <sebasdeldi123@gmail.com>
2016-01-30 15:43:18 -0500

    Implementating user creation

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  resources :users, only: [:new, :create]
+  resources :products
+  root 'products#index'
+
+end
+
+
   # get '/products', to: 'products#index'
   # get '/products/new', to: 'products#new', as: 'new_product'
   # post '/products', to: 'products#create'
@@ -6,8 +13,3 @@ Rails.application.routes.draw do
   # get '/products/:id/edit', to: 'products#edit', as:'edit'
   # patch '/products/:id', to: 'products#update'
   # delete '/products/:id', to: 'products#destroy'
-
-  resources :products
-  root 'products#index'
-
-end
