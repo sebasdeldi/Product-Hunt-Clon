bd131e80e6b8106d61a4c9390a5b3cca32f2c3a7
sebasdeldi <sebasdeldi123@gmail.com>
2016-01-30 12:29:36 -0500

    replacing routes for resorces

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160124015200) do
+ActiveRecord::Schema.define(version: 20160130120900) do
 
   create_table "products", force: :cascade do |t|
-    t.string   "name"
-    t.string   "url"
-    t.text     "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.string "name"
+    t.string "url"
+    t.text   "description"
   end
 
 end
